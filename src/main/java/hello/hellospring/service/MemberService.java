6e2883601b3658bfd8ad394fc89630ce1860db31
syc <syc@partdb.com>
2021-10-21 16:48:11 +0900

    feat AOP 적용 @syc

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -27,19 +27,10 @@ public class MemberService {
     // 회원 가입
     public Long join(Member member) {
 
-        long start = System.currentTimeMillis();
-
-        try {
-            // 같은 이름이 있는 중복회원 방지
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
-
+        // 같은 이름이 있는 중복회원 방지
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
 
     }
 
@@ -52,19 +43,13 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     // id를 통한 멤버 찾기
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 
 }
+
